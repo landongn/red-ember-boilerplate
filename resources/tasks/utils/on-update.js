aa7826ff3daffc7885ec70dd00934622a5c8a29f
Richard Herrera <rich@doctyper.com>
2012-09-14 11:44:03 -0700

    Create and return an API to run.

--- resources/tasks/utils/on-update.js
+++ resources/tasks/utils/on-update.js
@@ -1,6 +1,6 @@
 module.exports = function (cb) {
-	var fs = require("fs");
 	var pkg = require("./pkg");
+	var fs = require("fs");
 	var path = require("path");
 
 	function rename(obj, origVal, newVal) {
@@ -13,35 +13,44 @@ module.exports = function (cb) {
 		delete pkg.config[origVal];
 	}
 
-	// 2.5.0
-	(function () {
+	return {
+		versions : {
+			"2.5.0" : function () {
+				// resources/helpers/check-dependency.js -> resources/helpers/check-dependencies.js
+				var oldFile = path.join(__dirname, "../helpers/check-dependency.js");
 
-		// resources/helpers/check-dependency.js -> resources/helpers/check-dependencies.js
-		var oldFile = path.join(__dirname, "../helpers/check-dependency.js");
+				if (fs.existsSync(oldFile)) {
+					fs.unlinkSync(oldFile);
+				}
+			},
 
-		if (fs.existsSync(oldFile)) {
-			fs.unlinkSync(oldFile);
-		}
-	}());
+			"2.6.0" : function () {
+				if (pkg.config) {
 
-	// 2.6.0
-	(function () {
-		if (pkg.config) {
+					// pkg.config.rbp -> pkg.config.org
+					if ("rbp" in pkg.config) {
+						rename(pkg.config, "rbp", "org");
+					}
 
-			// pkg.config.rbp -> pkg.config.org
-			if ("rbp" in pkg.config) {
-				rename(pkg.config, "rbp", "org");
+					// pkg.config.installed_plugins -> pkg.config.installedPlugins
+					if ("installed_plugins" in pkg.config) {
+						rename(pkg.config, "installed_plugins", "installedPlugins");
+					}
+
+				}
+			}
+		},
+		run : function () {
+			var versions = this.versions,
+				version;
+
+			for (version in versions) {
+				versions[version]();
 			}
 
-			// pkg.config.installed_plugins -> pkg.config.installedPlugins
-			if ("installed_plugins" in pkg.config) {
-				rename(pkg.config, "installed_plugins", "installedPlugins");
+			if (cb) {
+				cb();
 			}
-
 		}
-	}());
-
-	if (cb) {
-		cb();
-	}
+	};
 };
