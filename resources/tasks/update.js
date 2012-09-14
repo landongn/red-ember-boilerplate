db9608ac211d69973a538a57a6e149f6e91cffcb
Richard Herrera <rich@doctyper.com>
2012-09-14 14:47:48 -0700

    Run on-update before continuing.

--- resources/tasks/update.js
+++ resources/tasks/update.js
@@ -3,32 +3,33 @@ module.exports = function (grunt) {
 
 	grunt.registerTask("update", "Update the boilerplate", function (plugin) {
 		var done = this.async();
-		var pkg = require("./utils/pkg");
-
-		// Sanity check
-		pkg.config.org = pkg.config.org || {};
-		pkg.config.org.repository = pkg.config.org.repository || {};
-
-		// Set plugin if not deflined
-		plugin = plugin || pkg.config.org.name;
-
-		var branch;
-		var bits = plugin.split("@");
-
-		if (bits.length === 1) {
-			plugin = bits[0];
-		} else {
-			plugin = bits[0];
-			branch = bits[1];
-		}
-
-		branch = branch || pkg.config.org.repository.branch || "master";
-		grunt.task.run("install:%p@%b:update".replace("%p", plugin).replace("%b", branch));
 
 		// Load on-update
 		var update = require("./utils/on-update");
 
 		update.run(function () {
+			var pkg = require("./utils/pkg");
+
+			// Sanity check
+			pkg.config.org = pkg.config.org || {};
+			pkg.config.org.repository = pkg.config.org.repository || {};
+
+			// Set plugin if not deflined
+			plugin = plugin || pkg.config.org.name;
+
+			var branch;
+			var bits = plugin.split("@");
+
+			if (bits.length === 1) {
+				plugin = bits[0];
+			} else {
+				plugin = bits[0];
+				branch = bits[1];
+			}
+
+			branch = branch || pkg.config.org.repository.branch || "master";
+			grunt.task.run("install:%p@%b:update".replace("%p", plugin).replace("%b", branch));
+
 			done();
 		});
 	});
