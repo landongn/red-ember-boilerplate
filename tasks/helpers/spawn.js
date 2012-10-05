be7704aa3bc74cf0eaea81bab427ba1aaa85c3ad
Richard Herrera <rich@doctyper.com>
2012-10-05 09:52:43 -0700

    JSHint.

--- tasks/helpers/spawn.js
+++ tasks/helpers/spawn.js
@@ -1,5 +1,7 @@
-/*jshint node:true*/
 module.exports = function (grunt) {
+	"use strict";
+
+	/*jshint node:true*/
 
 	grunt.registerHelper("spawn", function (opts) {
 		var cp = require("child_process");
