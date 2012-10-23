75d7d12760e5c05834c6063d48fa17f73b67c822
Richard Herrera <rich@doctyper.com>
2012-10-23 15:53:30 -0700

    Doc tweak.

--- test/tasks.js
+++ test/tasks.js
@@ -12,7 +12,7 @@ var test = path.join(cwd, "build");
 describe("Default Tasks", function () {
 
 	// Start a pristine Robyn project
-	// Expects a Robyn nest called 'robyn-test'
+	// This test expects a Robyn nest called 'robyn-test'
 	// Add: `robyn add robyn-test git://github.com/ff0000/robyn.git`
 	before(function (done) {
 		var afterBefore = function () {
