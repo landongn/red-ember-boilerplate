26a17fc74b72b6d74ce1735a01505689bd6eb59d
Richard Herrera <rich@doctyper.com>
2012-10-24 09:46:28 -0700

    Test exact output.

--- test/tasks.js
+++ test/tasks.js
@@ -30,20 +30,76 @@ describe("Default Tasks", function () {
 				nexpect.spawn("robyn", [
 					"init", "robyn-test", test,
 					"--name", "robynTest",
-					"--title", "Robyn Test",
-					"--all"
+					"--title", "Robyn Test"
 				], {
 					stripColors: true,
 					verbose: true
 				})
-				.wait("[*] Bootstrapping robyn")
-				.expect("Using: robyn-test at")
-				.wait("OK")
-				.expect("Adding robyn")
-				.wait("OK")
-				.expect("[*] Project shell complete.")
-				.wait("[*] You should edit your package.json and fill in your project details.")
+				.wait("[*] Project shell complete.")
+
+				.expect('Running "start" task')
+
+				.expect('[*] Starting the party')
+				.expect('    Installing npm modules').wait('OK')
+
+				.expect("[*] Checking for available plugins.")
+				.expect("    Found the following: caboose, modernizr, red-start, rosy, statix")
+
+				.expect("[*] Checking param overrides.")
+				.expect("    project name: robynTest, project title: Robyn Test")
+
+				.expect("Please answer the following:")
+
+				.expect("[?] Would you like to add caboose (RED Interactive's internal SASS + Compass framework)? (Y/n) ")
+				.sendline("Y")
+
+				.expect("[?] Would you like to add modernizr (Build out a lean, mean Modernizr machine.)? (Y/n) ")
+				.sendline("Y")
+
+				.expect("[?] Would you like to add red-start (Easy creation of Django projects and applications based the layout used at RED Interactive Agency.)? (Y/n) ")
+				.sendline("Y")
+
+				.expect("[?] Would you like to add rosy (An inheritable / extendable JavaScript framework.)? (Y/n) ")
+				.sendline("Y")
+
+				.expect("[?] Would you like to add statix (Statix is a static website generator.)? (Y/n) ")
+				.sendline("Y")
+
+				.expect("[?] Do you need to make any changes to the above before continuing? (y/N) ")
+				.sendline("N")
+
+				.expect("[*] Stored and updated your project variables.")
+
+				.expect("[+] Installing caboose via https://github.com/ff0000/caboose.git")
+				.expect("    Cloning repository").wait("OK")
+
+				.expect("    Copying files into project").wait("OK")
+
+				.expect("    Installing bundle. This may take a minute").wait("OK")
+
+				.expect("[+] Installing modernizr via .robyn/plugins/modernizr")
+				.expect("    Installing npm modules").wait("OK")
+				.expect("    Copying files into project").wait("OK")
+
+				.expect("[+] Installing red-start via .robyn/plugins/red-start")
+				.expect("    Copying files into project").wait("OK")
+				.expect("    Creating a new red-start project").wait("OK")
+				.expect("    Creating a virtualenv. This may take a minute").wait("OK")
+
+				.expect("[+] Installing rosy via https://github.com/ff0000/rosy.git")
+				.expect("    Installing npm modules").wait("OK")
+				.expect("    Cloning repository").wait("OK")
+				.expect("    Copying files into project").wait("OK")
+				.expect("    Installing external libraries").wait("OK")
+
+				.expect("[+] Installing statix via .robyn/plugins/statix")
+				.expect("    Copying files into project").wait("OK")
+
+				.expect("[*] You should edit your package.json and fill in your project details.")
 				.expect("[*] All done! Commit you changes and you're on your way.")
+
+				.expect("Done, without errors.")
+
 				.run(done);
 			}
 		};
