d8fcce90c1a4c5bca397e303b71999ec8e261715
Richard Herrera <rich@doctyper.com>
2012-10-23 19:38:04 -0700

    Move node/print definition inside requirejs multitask. Fixes bug with grunt build task.

--- plugins/rosy/tasks/helpers/require.js
+++ plugins/rosy/tasks/helpers/require.js
@@ -37,37 +37,38 @@ module.exports = function (grunt) {
 		});
 	});
 
-	// TODO: extend this to send build log to grunt.log.ok / grunt.log.error
-	// by overriding the r.js logger (or submit issue to r.js to expand logging support)
-	requirejs.define("node/print", [], function () {
-		return function print(msg) {
-			if (msg.substring(0, 5) === "Error") {
-				grunt.log.errorlns(msg);
-				grunt.fail.warn("RequireJS failed.");
-			} else {
-				msg = msg.replace(new RegExp(absPath, "mg"), "");
-
-				if (msg.indexOf(lineBreak) !== -1) {
-					msg = msg.split(lineBreak);
-					msg.shift();
-
-					grunt.log.subhead("Built with the following modules:".grey);
-
-					msg = msg.join("");
-				}
-
-				if (msg.indexOf("Tracing dependencies for:") !== -1) {
-					grunt.log.subhead(msg.trim().grey);
-				} else if (msg.indexOf("Uglifying file:") !== -1) {
-					grunt.log.writeln(msg.trim().green);
-				} else {
-					grunt.log.writeln(msg.trim().grey);
-				}
-			}
-		};
-	});
-
 	grunt.registerMultiTask("requirejs", "Build a RequireJS project.", function () {
+
+		// TODO: extend this to send build log to grunt.log.ok / grunt.log.error
+		// by overriding the r.js logger (or submit issue to r.js to expand logging support)
+		requirejs.define("node/print", [], function () {
+			return function print(msg) {
+				if (msg.substring(0, 5) === "Error") {
+					grunt.log.errorlns(msg);
+					grunt.fail.warn("RequireJS failed.");
+				} else {
+					msg = msg.replace(new RegExp(absPath, "mg"), "");
+
+					if (msg.indexOf(lineBreak) !== -1) {
+						msg = msg.split(lineBreak);
+						msg.shift();
+
+						grunt.log.subhead("Built with the following modules:".grey);
+
+						msg = msg.join("");
+					}
+
+					if (msg.indexOf("Tracing dependencies for:") !== -1) {
+						grunt.log.subhead(msg.trim().grey);
+					} else if (msg.indexOf("Uglifying file:") !== -1) {
+						grunt.log.writeln(msg.trim().green);
+					} else {
+						grunt.log.writeln(msg.trim().grey);
+					}
+				}
+			};
+		});
+
 		var _ = grunt.util._;
 		var kindOf = grunt.util.kindOf;
 		var options = grunt.helper("options", this, {
