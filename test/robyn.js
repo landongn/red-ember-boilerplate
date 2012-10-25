087d7d641407aba5c1993c99e7371c84e77d73ea
Richard Herrera <rich@doctyper.com>
2012-10-25 14:50:35 -0700

    .

--- test/robyn.js
+++ test/robyn.js
@@ -11,34 +11,35 @@ module.exports = {
 
 		var afterBefore = function () {
 			if (fs.existsSync(test)) {
-				callback();
-			} else {
-				nexpect.spawn("robyn", [
-					"init", "robyn-test", test,
-					"--name", "robynTest",
-					"--title", "Robyn Test",
-					"--all"
-				], {
-					stripColors: true,
-					verbose: true
-				})
-				.wait("[*] Bootstrapping robyn")
-				.expect("Using: robyn-test at")
-				.wait("OK")
-				.expect("Adding robyn")
-				.wait("OK")
-				.expect("[*] Project shell complete.")
-				.wait("[*] You should edit your package.json and fill in your project details.")
-				.expect("[*] All done! Commit you changes and you're on your way.")
-				.run(function (err) {
-					if (err) {
-						console.error(err);
-						process.exit();
-					}
-
-					callback();
-				});
+				var wrench = require("wrench");
+				wrench.rmdirSyncRecursive(test);
 			}
+
+			nexpect.spawn("robyn", [
+				"init", "robyn-test", test,
+				"--name", "robynTest",
+				"--title", "Robyn Test",
+				"--all"
+			], {
+				stripColors: true,
+				verbose: true
+			})
+			.wait("[*] Bootstrapping robyn")
+			.expect("Using: robyn-test at")
+			.wait("OK")
+			.expect("Adding robyn")
+			.wait("OK")
+			.expect("[*] Project shell complete.")
+			.wait("[*] You should edit your package.json and fill in your project details.")
+			.expect("[*] All done! Commit you changes and you're on your way.")
+			.run(function (err) {
+				if (err) {
+					console.error(err);
+					process.exit();
+				}
+
+				callback();
+			});
 		};
 
 		nexpect.spawn("robyn", ["list", "robyn-test"], {
