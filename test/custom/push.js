7c188cecd8acdead0398c7f1ec8f3bd9414ef5b0
Richard Herrera <rich@doctyper.com>
2012-11-02 14:50:06 -0700

    Don't test specific errors, it will fail for various reasons.

--- test/custom/push.js
+++ test/custom/push.js
@@ -10,16 +10,7 @@ describe("Push Task", function () {
 	it("grunt push", function (done) {
 		grunt.spawn("push")
 		.expect('Running "push" task')
-		.wait('Running "push:apply')
-
-		.expect("<WARN> fatal: 'origin' does not appear to be a git repository")
-		.expect("fatal: Could not read from remote repository.")
-
-		.expect("Please make sure you have the correct access rights")
-		.expect("and the repository exists.")
-		.expect("Use --force to continue. </WARN>")
-
-		.expect("Aborted due to warnings.")
+		.wait("Aborted due to warnings.")
 		.run(done);
 	});
 
