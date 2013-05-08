07e9a7c14fcf317dd5bbd020c544552779d764ae
Richard Herrera <rich@doctyper.com>
2013-05-08 07:20:28 -0700

    Tmux doesn't like being a child process. This is going to be a problem.

--- plugins/tmux/tasks/tmux.js
+++ plugins/tmux/tasks/tmux.js
@@ -3,16 +3,15 @@ module.exports = function (grunt) {
 	"use strict";
 
 	grunt.registerTask("tmux", "Use tmux to spool up all available server / watch tasks", function () {
+		var done = this.async();
 		var cp = require("child_process");
-
-		var child = cp.spawn("bundle", [
-			"exec",
-			"tmuxinator",
-			"start",
+		var args = [
+			"bundle", "exec",
+			"tmuxinator", "start",
 			grunt.template.process("<%= meta.projectName %>")
-		], {
-			stdio: "inherit"
-		});
+		];
+
+		cp.exec(args.join(" "), done);
 	});
 
 };
