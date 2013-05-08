4c9c6bc7a8a11ea0afd9e5f1f18878f223eee145
Richard Herrera <rich@doctyper.com>
2013-05-08 10:35:16 -0700

    Attempt to run tmux in a detached state.

--- plugins/tmux/tasks/tmux.js
+++ plugins/tmux/tasks/tmux.js
@@ -3,15 +3,16 @@ module.exports = function (grunt) {
 	"use strict";
 
 	grunt.registerTask("tmux", "Use tmux to spool up all available server / watch tasks", function () {
-		var done = this.async();
 		var cp = require("child_process");
-		var args = [
-			"bundle", "exec",
-			"tmuxinator", "start",
-			grunt.template.process("<%= meta.projectName %>")
-		];
 
-		cp.exec(args.join(" "), done);
+		var child = cp.spawn("bundle", [
+			"exec", "tmuxinator", "start",
+			grunt.template.process("<%= meta.projectName %>")
+		], {
+			detached: true
+		});
+
+		child.unref();
 	});
 
 };
