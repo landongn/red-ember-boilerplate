068761a6aef0849a03dd2ea958cf9240b1b7b798
Richard Herrera <rich@doctyper.com>
2013-05-13 15:24:42 -0700

    Add bower integration.

--- plugins/rosy/tasks/rosy.js
+++ plugins/rosy/tasks/rosy.js
@@ -4,40 +4,79 @@
 module.exports = function (grunt) {
 
 	var fs = require("fs"),
+		cwd = process.cwd(),
 		path = require("path"),
 		rosy = require(path.join(__dirname, "..", "plugin.json")),
 		output = path.join("project", "static", "js"),
 		source = rosy.config.scope;
 
-	// Project configuration.
-	grunt.config.set("requirejs.desktop", {
-		options : {
-			mainConfigFile : path.join(source, "config.js"),
-			urlArgs : null,
-			include : ["config.js"],
-			paths : {
-				"jquery": "empty:"
-			},
-			generateSourceMaps : true,
-			preserveLicenseComments : false,
-			optimize : "uglify2",
-			uglify2 : {
-				warnings : true
-			},
-			out : path.join(output, "site.min.js"),
-			name : grunt.template.process("<%= meta.projectName %>") + "/Site",
-			skipModuleInsertion : true
+	var logPlugins = function (filtered) {
+		for (var i = 0, j = filtered.length; i < j; i++) {
+			var plug = filtered[i];
+			grunt.log.writeln("[+] ".grey + "%n".replace("%n", plug.shortname).cyan);
 		}
+	};
+
+	grunt.registerTask("rosy", "Install Rosy components", function (components) {
+		var done = this.async();
+		var https = require("https");
+
+		components = (components || "").split(",");
+
+		var url = "https://api.github.com/orgs/rosy-components/repos";
+
+		https.get(url, function (res) {
+			var data = [];
+
+			res.on("data", function (chunk) {
+				data.push(chunk);
+			});
+
+			res.on("end", function () {
+				var repos = JSON.parse(data.join("")).map(function (repo) {
+					return {
+						clone_url : repo.clone_url,
+						name : repo.name,
+						shortname : repo.name.replace(/^rosy\-/, "")
+					};
+				});
+
+				if (!components[0]) {
+					grunt.log.writeln();
+					grunt.log.writeln("[*] ".grey + "Available plugins:".magenta);
+
+					logPlugins(repos);
+
+					grunt.log.writeln();
+					grunt.log.writeln("Install plugins with grunt install:rosy-plugin-name");
+				} else {
+					var bower = require("bower");
+
+					// Change directory to Rosy root
+					process.chdir(path.join(cwd, source));
+
+					var title;
+
+					var install = repos.filter(function (repo) {
+						return components.indexOf(repo.shortname) !== -1;
+					}).map(function (repo) {
+						return repo.clone_url;
+					});
+
+					bower.commands.install(install).on("data", function (data) {
+						console.log("    " + data);
+					}).on("end", function (data) {
+						if (!grunt.option("verbose")) {
+							process.stdout.write("OK".green);
+						}
+
+						process.chdir(cwd);
+						return done;
+					});
+				}
+			});
+
+		});
 	});
 
-	grunt.config.set("watch.requirejs", {
-		files: path.join(source, "**", "*[^.min].js"),
-		tasks: ["requirejs:reload"],
-		options: {
-			interrupt: true,
-			livereload: true
-		}
-	});
-
-	grunt.config.set("build.requirejs", ["requirejs"]);
 };
