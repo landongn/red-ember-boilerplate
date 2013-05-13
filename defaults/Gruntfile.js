1406cfb5856c8bf7309f8f0c958a8b44106a36db
Richard Herrera <rich@doctyper.com>
2013-05-13 12:43:04 -0700

    Be nice to Windows.

--- defaults/Gruntfile.js
+++ defaults/Gruntfile.js
@@ -25,7 +25,7 @@ module.exports = function (grunt) {
 			files = grunt.file.expand(path.join(dir, "*"));
 
 		if (!files.length) {
-			var d = dir.replace(cwd + "/", "");
+			var d = dir.replace(cwd + path.sep, "");
 
 			var warn = [
 				"%s is not yet initialized".replace("%s", d),
